f5c30b86dfd61ea0df378da86ef03eb98fffa9f8
rafneto <39274216+rafneto@users.noreply.github.com>
2024-04-11 17:04:21 +0100

    Added Thread pooling
    Allows you to scan multiple ports concurrently, significantly reducing the overall execution time

--- scanner.py
+++ scanner.py
@@ -1,17 +1,37 @@
-from socket import *
 import time
+from concurrent.futures import ThreadPoolExecutor
+from socket import AF_INET, SOCK_STREAM, socket
 
-start_time=time.time()
 
-if __name__ == '__main__':
+def scan_port(target, port):
+    """
+    Scan a single port on the target host.
+    Args:
+        target (str): The IP address of the target host.
+        port (int): The port number to scan.
+    """
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.settimeout(0.3)
+    connection = sock.connect_ex((target, port))
+    if connection == 0:
+        print("Port %d: OPEN" % (port,))
+    sock.close()
+
+
+def main():
+    """
+    Main function to initiate port scanning.
+    This function prompts the user to enter the IP address of the target host and starts scanning ports.
+    It utilizes ThreadPoolExecutor to concurrently scan ports for faster execution.
+    """
     target = input("Enter the host IP adrress to be scanned: ")
     print("Starting scan host: ", target)
-    
-    for i in range(1, 10000):
-        sock = socket(AF_INET, SOCK_STREAM)
-        sock.settimeout(.3)
-        connection = sock.connect_ex((target, i))
-        if (connection == 0):
-            print("Port %d: OPEN" % (i,))
-        sock.close()
-print("Time:", time.time() - start_time)
+
+    with ThreadPoolExecutor(max_workers=1000) as executor:
+        futures = [executor.submit(scan_port, target, port) for port in range(1, 10000)]
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print(f"Time: {round(time.time() - start_time, 2)} secs")
